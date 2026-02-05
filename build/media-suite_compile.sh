48105864768d0241428d1cb2f38ca865ed20c699
netony <hevc1@foxmail.com>
2026-02-05 11:36:27 +0800

    更新VapourSynth至R73

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2044,9 +2044,9 @@ _vapoursynth_install() {
         return 1
     fi
     do_pacman_install tools
-    _python_ver=3.12.10
-    _python_lib=python312
-    _vsver=72
+    _python_ver=3.13.11
+    _python_lib=python313
+    _vsver=73
     _check=("lib$_python_lib.a")
     if files_exist "${_check[@]}"; then
         do_print_status "python $_python_ver" "$green" "Up-to-date"
@@ -2057,7 +2057,7 @@ _vapoursynth_install() {
         do_checkIfExist
     fi
 
-    _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}.h)
+    _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}4.h)
     if pc_exists "vapoursynth = $_vsver" && files_exist "${_check[@]}"; then
         do_print_status "vapoursynth R$_vsver" "$green" "Up-to-date"
     elif do_wget "https://github.com/vapoursynth/vapoursynth/releases/download/R$_vsver/VapourSynth${bits%bit}-Portable-R$_vsver.zip"; then
@@ -2065,7 +2065,7 @@ _vapoursynth_install() {
         do_install sdk/include/vapoursynth/*.h include/vapoursynth/
 
         # Extract the .dll from the pip wheel
-        log "7z" 7z e -y -aoa wheel/vapoursynth-$_vsver-cp${_python_lib:6:3}-abi3-win_amd64.whl \
+        log "7z" 7z e -y -aoa wheel/vapoursynth-$_vsver-cp312-abi3-win_amd64.whl \
             vapoursynth-$_vsver.data/data/Lib/site-packages/vapoursynth.dll
 
         create_build_dir
